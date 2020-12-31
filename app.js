7babaaba3f905c3ab18aab3f917ff0d77d1f3fc0
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2020-12-31 22:20:57 +0530

    added demo attributes

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works for me!'
-    });
+    res.status(200).json(
+        {
+        message: 'It works for me!',
+        id: '123',
+        sex: 'm'
+        }
+    );
 });
 
 module.exports = app;
