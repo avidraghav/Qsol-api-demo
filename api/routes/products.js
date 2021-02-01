67e420eb17c3398e6a608b04bdccf9ee82250f49
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2021-02-01 14:07:02 +0530

    added url attribute

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,21 +5,16 @@ router.get('/', (req, res, next) =>{
     res.status(200).json({
         'items':[
             {
-                name: 'datesheet 1',
+                name: 'B.Tech 5th semester January 2021',
+                url: 'https://firebasestorage.googleapis.com/v0/b/new-database-d16b8.appspot.com/o/IN%2FKU%2FCS%2F05%2FDatesheet%2FEngineering%20V%20Semester%20Examinations%20Jan%202021.pdf?alt=media&token=c38ca851-1060-47d1-adae-0015876927e7',
                 id: '1',
             },
             {
-                name: 'datesheet 2',
-                id: '2',
-            },
-            {
-                name: 'datesheet 3',
-                id: '3',
-            },
-            {
-                name: 'datesheet 4',
-                id: '4',
+                name: 'B.Tech 7th semester January 2021',
+                url: 'https://firebasestorage.googleapis.com/v0/b/new-database-d16b8.appspot.com/o/IN%2FKU%2FCS%2F07%2FDatesheet%2FEngineering%20VII%20Semester%20Examinations%20Jan%202021.pdf?alt=media&token=43357cec-58c9-45b7-b0af-f608cae6db95',
+                id: '1',            
             }
+           
         ]
      
     });
